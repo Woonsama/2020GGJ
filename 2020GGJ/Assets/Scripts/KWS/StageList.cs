83a2ed020dd27de3760623f90c83b0cd23ee0dce
Wonnsama <kws4510@naver.com>
2020-01-31 22:18:22 +0900

    테스트

---
+++ 2020GGJ/Assets/Scripts/KWS/StageList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+
+public class Stage
+{
+    public GameObject stageUI;
+    public int stageNum;
+    public bool isStageLocked;
+}
+
+public class StageList : MonoBehaviour
+{
+    public Stage[] stageList;
+
+    void Update()
+    {
+
+    }
+}
